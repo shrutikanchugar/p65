91bb6e2e171586cc208318cbb5acddbaa3acbb5d
shrutikanchugar <68994100+shrutikanchugar@users.noreply.github.com>
2022-07-08 19:28:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+console.log("hello world, this is JS class");
+
+var first_name = "Akshat";
+console.log(first_name);
+
+var last_name = "Kumar";
+console.log(last_name);
+
+console.log(first_name,last_name);
+full_name = first_name.concat(last_name);
+console.log(full_name)
+
+var len = first_name.length;
+console.log(len)
+
+var name = "I am Akshat Kumar";
+console.log(name)
+
+name2 = name.replace("Kumar", "K");
+console.log(name2)
+
+up = "AKSHAT";
+console.log(up)
+
+low = up.toLowerCase();
+console.log(low)
+
+upper = low.toUpperCase();
+console.log(upper)
+
+var square = Math.sqrt(81);
+console.log(square)
+
+var max1 = Math.max(23,5869,12,1);
+console.log(max1)
+
+var min1 = Math.min(23,5869,12,1)
+console.log(min1)
